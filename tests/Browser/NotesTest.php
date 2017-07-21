4d23c00915ddbad271d6571df154bc9fb0584764
begimov <begimov@gmail.com>
2017-07-21 17:11:00 +0300

    new note saving, adding related tests added

--- tests/Browser/NotesTest.php
+++ tests/Browser/NotesTest.php
@@ -7,6 +7,9 @@ use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\User;
 use Tests\Browser\Pages\SignUpPage;
+use Tests\Browser\Pages\NotesPage;
+
+const DELAY = 500;
 
 class NotesTest extends DuskTestCase
 {
@@ -26,6 +29,51 @@ class NotesTest extends DuskTestCase
                 ->assertSee('Untitled')
                 ->assertValue('#title', '')
                 ->assertValue('#body', '');
+        });
+    }
+
+    public function test_user_can_save_new_note()
+    {
+        $user = factory(User::class)->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit(new NotesPage)
+                ->enterNote('First', 'BodyFirst')
+                ->saveNote()
+                ->pause(DELAY)
+                ->assertSeeIn('.alert', 'Your new note')
+                ->assertSeeIn('.notes', 'You have 1 note')
+                ->assertSeeIn('.notes', 'First')
+                ->assertInputValue('#title', 'First')
+                ->assertInputValue('#body', 'BodyFirst');
+        });
+    }
+
+    public function test_user_can_see_word_count()
+    {
+        $user = factory(User::class)->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit(new NotesPage)
+                ->enterNote('First', 'BodyFirst')
+                ->assertSee('Word count: 1');
+        });
+    }
+
+    public function test_user_can_start_new_note()
+    {
+        $user = factory(User::class)->create();
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->visit(new NotesPage)
+                ->enterNote('First', 'BodyFirst')
+                ->saveNote()
+                ->pause(DELAY)
+                ->clickLink('Create new note')
+                ->pause(DELAY)
+                ->assertSee('A fresh note')
+                ->assertInputValue('#title', '')
+                ->assertInputValue('#body', '');
         });
     }
 }
